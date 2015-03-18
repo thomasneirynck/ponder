0c0836db12e5dd7fcdcd862558a7b3f50e3d081f
trgn <thomas@neirynck.us>
2015-03-18 00:59:13 -0400

    add some bmu stuff

--- src/SOM.js
+++ src/SOM.js
@@ -1,15 +1,16 @@
 define(["type"], function (type) {
 
-    function compareLowest(){
+    function compareLowest() {
 
     }
+
     compareLowest.feature = null;
     compareLowest.i = -1;
 
-    function distance(codebook, ci, feature, vi,_codebookSize) {
+    function distance(vector1, i1, vector2, i2, codebookSize) {
         var sum = 0;
-        for (var i = 0; i < this._codeBookSize; i += 1){
-
+        for (var i = 0; i < codebookSize; i += 1) {
+            sum += Math.pow(vector1[i1 + i] - vector2[i2 + i], 2)
         }
         return Math.sqrt(sum);
     }
@@ -20,33 +21,44 @@ define(["type"], function (type) {
             this._width = options.width;
             this._height = options.height;
             this._codeBookSize = options.codeBookSize;
-            this._data = new Array(this._width * this._height * this._codeBookSize);
-        },
+            this._neuralWeights = new Array(this._width * this._height * this._codeBookSize);
 
-        initializeCodebook: function (referenceFeatures, count) {
-
+            for (var i = 0; i < this._neuralWeights.length; i += 1) {
+                this._neuralWeights[i] = Math.random();
+            }
         },
 
         learn: function (feature, i) {
         },
 
-        train: function(dataIterator){
-
-        },
-
-        forEachCodeBook: function(){
+        train: function (dataIterator) {
         },
 
         reduceCodeBook: function (fold, accumulator) {
             var x, y;
-            for (var i = 0; i < this._data.length; i += this._codeBookSize) {
-                accumulator = fold(accumulator, this._data, x, y, this._codeBookSize);
+            for (var i = 0; i < this._neuralWeights.length; i += this._codeBookSize) {
+                accumulator = fold(accumulator, this._neuralWeights, x, y, this._codeBookSize);
             }
             return accumulator;
         },
 
-        bmu: function (feature, i, out) {
-            this.reduceCodeBook(feature);
+        toIndex: function (x, y) {
+            return ((this._width * x) + y) * this._codeBookSize;
+        },
+
+        bmu: function (feature, fi, out) {
+            var minDistance = Infinity;
+            var minI, dist;
+            for (var i = 0; i < this._neuralWeights.length; i += this._codeBookSize) {
+                dist = distance(this._neuralWeights, i, feature, fi,this._codeBookSize);
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    minI = i;
+                }
+            }
+            out.i = minI;
+            out.x = Math.floor(minI / this._codeBookSize / this._width);
+            out.y = (minI / this._codeBookSize) % this._width;
         },
 
         distance: function (codebook, i) {
@@ -55,11 +67,11 @@ define(["type"], function (type) {
         write: function () {
         },
 
-        draw: function(context2d){
+        draw: function (context2d) {
 
         },
 
-        findXY: function(codebook, i){
+        findXY: function (codebook, i) {
         }
 
     });
--- tests/SOMTest.js
+++ tests/SOMTest.js
@@ -7,14 +7,28 @@ define([
     //run "./node_modules/.bin/intern-client" config=tests/intern suites=tests/all
     tdd.suite("test", function () {
         tdd.test("constructor", function () {
-            var aTree = new SOM({
-                width: 10,
-                height: 10,
+            var som = new SOM({
+                width: 4,
+                height: 4,
                 codeBookSize: 3
             });
-            assert.equal(aTree.constructor, SOM, "should be vanilla prototype inheritance");
+            assert.equal(som.constructor, SOM, "should be vanilla prototype inheritance");
         });
 
+        tdd.test("bmu", function () {
+            var som = new SOM({
+                width: 2,
+                height: 3,
+                codeBookSize: 3
+            });
+            som._neuralWeights = [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+            var out = {};
+            som.bmu([0, 0, 0, 1, 1, 1], 3, out);
+            assert.equal(out.i, 3, "should be at 4th index in data");
+            assert.equal(out.x, 0, "first row");
+            assert.equal(out.y, 1, "second element");
+        });
     });
 
 
