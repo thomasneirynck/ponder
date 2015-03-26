99f54724029615d2b2bae863c6a6b2e276eb676a
trgn <thomas@neirynck.us>
2015-03-25 23:35:50 -0400

    rockin with papaparse

--- src/app.js
+++ src/app.js
@@ -4,18 +4,47 @@ require.config({
         'ponder': 'src',
         'type': "bower_components/type/type",
         'Promise': "bower_components/Promise/Promise",
-        'Papa': "vendor/papaparse"
+        'Papa': "vendor/papaparse",
+        '$': "bower_components/jquery/dist/jquery"
     },
     shim: {
         Papa: {
-            exports: "Papa"
+            exports: "Papa",
+            init: function(){
+                Papa.SCRIPT_PATH = "vendor/papaparse.js";
+            }
+        },
+        $: {
+            exports: "$"
         }
     }
 });
 
-require(["Papa"], function (Papa) {
+require(["Papa", "$"], function (Papa, $) {
 
 
     console.log("args", arguments);
+
+    document
+        .getElementById("fileSelect")
+        .addEventListener("change", function (event) {
+            var file = event.target.files[0];
+            if (!file) {
+                return;
+            }
+
+            Papa.parse(file, {
+                worker: true,
+                header: true,
+                complete: function (parsedObject) {
+                    console.log("parsed!", parsedObject);
+                },
+                error: function () {
+                    alert("cant parse file");
+                }
+            });
+
+        }, false);
+
 
 });
