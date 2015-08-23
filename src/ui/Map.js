991823107aaf95abc09c1065a41534dff5d71261
trgn <thomas@neirynck.us>
2015-08-23 17:17:21 -0400

    fiddle with map

--- src/ui/Map.js
+++ src/ui/Map.js
@@ -1,6 +1,23 @@
-define([], function(){
+define(["type", "Evented"], function (type, Evented) {
 
 
+    return type(Object.prototype, Evented.prototype, {
+
+        constructor: function Map() {
+            Evented.call(this);
+
+
+        },
+
+        addLayer: function(){
+
+        },
+
+        invalidate: function(){
+            
+        }
+
+    });
 
 
 });
