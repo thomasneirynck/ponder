ff0a8b7a96cc2433e59d6f7ed4c04024928bef05
Thomas Neirynck <thomas@elastic.co>
2016-10-07 21:35:39 -0400

    isolate datatable/fix issues

--- app/www/js/ponder/app.js
+++ app/www/js/ponder/app.js
@@ -47,41 +47,7 @@ require([
 ], function (DataSelector, Map, UMatrixTerrainLayer, BMULayer, AreaSelectLayerController, HoverController, SelectController, BMUSelector, BMUSelectionHistory, util, appApi, introJs, jquery) {
 
 
-    console.log('creating som options', arguments);
-
-    //----------------------------------
-    //FUCKING UP!!!!
-    var params = {
-        somWorkerScriptPath: /**{{SOM_SCRIPT_PATH}}*/null/**{{SOM_SCRIPT_PATH}}*/,
-        papaParseScriptPath: /**{{PAPA_PARSE_SCRIPT_PATH}}*/require.toUrl("Papa") + ".js"/**{{PAPA_PARSE_SCRIPT_PATH}}*/,
-        container: document.body,
-        mapTool: 'mapToolContainer',
-        mapTableToggle: 'toggle'
-    };
-    var containerNode = typeof params.container === 'string' ? document.getElementById(params.container) : params.container;
-    var mapToolNode = typeof params.mapTool === 'string' ? document.getElementById(params.mapTool) : params.mapTool;
-    var mapTableToggleNode = typeof params.mapTableToggle === 'string' ? document.getElementById(params.mapTableToggle) : params.mapTableToggle;
-    containerNode.addEventListener("contextmenu", function (e) {
-        e.preventDefault();
-    });
-    //----------------------------------
-
-
-    var oldDisplayMapToolDisplay = mapToolNode.style.display;
-    var oldDisplayToggleDisplay = mapTableToggleNode.style.display;
-
-    var somHandle;
-
-
-    Papa.SCRIPT_PATH = params.papaParseScriptPath;
-
-
-    function throwError(error) {
-        console.error(error);
-        console.error(error.stack);
-        throw error;
-    }
-
+    Papa.SCRIPT_PATH = /**{{PAPA_PARSE_SCRIPT_PATH}}*/require.toUrl("Papa") + ".js"/**{{PAPA_PARSE_SCRIPT_PATH}}*/;
 
     var tableContainer = document.getElementById("tableContainer");
     var mapContainer = document.getElementById("map");
@@ -131,140 +97,18 @@ require([
         dataSelector.destroy();
         jquery("#welcome").hide();
 
-
         //figure out title
         var title = util.getParameterByName("title") ? util.getParameterByName("title") : dataTable.getName();
         document.getElementById("title-blurb").innerHTML = title.toUpperCase();
 
-        var somTrainingData = dataTable.createSOMTrainingData();
-
-        var map, uMatrixLayer;
-        if (somHandle) {
-            somHandle.kill();
-            somHandle = null;
-            if (map) {
-                map.destroy();
-            }
-            map = null;
-        }
-
-        var waitingDiv = document.createElement("div");
-        waitingDiv.setAttribute("data-ponder-type", "progress_indicator");
-        var spinnerIcon = document.createElement("img");
-        spinnerIcon.src = "images/ajax-loader.gif";
-        waitingDiv.appendChild(spinnerIcon);
-        var waitingDivText = document.createElement("span");
-        waitingDivText.innerHTML = "Estimating progress...";
-        waitingDiv.appendChild(waitingDivText);
-        document.getElementById("waiting").appendChild(waitingDiv);
-
-
         appApi.createSOM({
-            somTrainingData: somTrainingData,
-            somWorkerScriptPath: params.somWorkerScriptPath
-        })
-        .then(function (aSomHandle) {
-            somHandle = aSomHandle;
-            return somHandle.trainMap();
-        }, throwError)
-        .then(function () {
-            return somHandle.uMatrixNormalized();
-        }, throwError, function (payload) {
-            //todo!!!! YOU HACKED THE PROMISE DEPENDENCY WHICH HAD A BUG!!!!!!! FIX IT!!!!
-            waitingDivText.innerHTML = "Making map..." + Math.round(payload.progress * 100) + "%";
-        })
-        .then(function (successData) {
+            table: dataTable,
+            somWorkerScriptPath: /**{{SOM_SCRIPT_PATH}}*/null/**{{SOM_SCRIPT_PATH}}*/
+        });
 
-            jquery("#map").show();
-            document.getElementById("mapToolContainer").style.display = oldDisplayMapToolDisplay;
-            mapContainer.style.display = "block";
-
-
-            map = new Map("map", somHandle.width, somHandle.height);
-
-            uMatrixLayer = new UMatrixTerrainLayer("ease", "easeReadout");
-            uMatrixLayer.setUMatrixData(successData.uMatrix, somHandle.width, somHandle.height);
-            map.addLayer(uMatrixLayer);
-
-            waitingDivText.innerHTML = "Finding locations ...";
-            document.getElementById("center").appendChild(waitingDiv);
-
-
-            return somHandle.bmus();
-        }, throwError)
-        .then(function (bmuResult) {
-
-            document.getElementById("toggle").style.display = oldDisplayToggleDisplay;
-
-            waitingDiv.parentNode.removeChild(waitingDiv);
-            document.getElementById("waiting").style.display = "none";
-            document.getElementById("center").style.overflow = "auto";
-
-
-            //bmus
-            var bmuLayer = new BMULayer("label", "class", "size", "sizeEasing", "legend", dataTable, bmuResult.locations);
-            map.addLayer(bmuLayer);
-
-            var areaSelectLayerController = new AreaSelectLayerController();
-            areaSelectLayerController.setOnMap(map);
-
-            var bmuSelector = new BMUSelector(areaSelectLayerController, bmuLayer, somHandle, "table", "summary");
-            bmuSelector.on("RowSelection", function (object) {
-                bmuLayer.highlight([object.index]);
-            });
-
-
-            var bmuSelectionHistory = new BMUSelectionHistory("selectionHistory", bmuSelector, map, areaSelectLayerController.isActive.bind(areaSelectLayerController), [uMatrixLayer, areaSelectLayerController], "stripSelected");
-            bmuSelector.selectAll();
-
-
-            var hoverController = new HoverController();
-            hoverController.setOnMap(map);
-
-            var selectController = new SelectController();
-            selectController.setOnMap(map);
-
-
-        }, throwError)
-        .then(function () {
-
-            //start intro
-            if (getCookie("ponder-intro") === "1") {
-                return;
-            }
-
-            var intro = introJs();
-            setCookie("ponder-intro", "1", 100);
-            intro.start();
-
-
-        })
-        .then(Function.prototype, throwError);
     });
 
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + "; " + expires;
-    }
 
-    function getCookie(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') {
-                c = c.substring(1);
-            }
-
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length, c.length);
-            }
-
-        }
-        return "";
-    }
 
 
 });
--- src/appApi.js
+++ src/appApi.js
@@ -14,6 +14,13 @@ define([
 ], function (SOMFactory, Map, UMatrixTerrainLayer, BMULayer, AreaSelectLayerController, HoverController, SelectController, BMUSelector, BMUSelectionHistory, util, appApi, jquery) {
 
 
+  function throwError(error) {
+    console.error(error);
+    console.error(error.stack);
+    throw error;
+  }
+
+
   /**
    * Public API to embed this app inside another web-app.
    * .. starting this so we can
@@ -22,19 +29,154 @@ define([
 
     createSOM: function (params) {
 
-      var somTrainingData = params.somTrainingData;
+      params.container = document.body;
+      params.mapTool = 'mapToolContainer';
+      params.mapTableToggle = 'toggle';
+
+      var containerNode = typeof params.container === 'string' ? document.getElementById(params.container) : params.container;
+      var mapToolNode = typeof params.mapTool === 'string' ? document.getElementById(params.mapTool) : params.mapTool;
+      var mapTableToggleNode = typeof params.mapTableToggle === 'string' ? document.getElementById(params.mapTableToggle) : params.mapTableToggle;
+      containerNode.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+      });
+      //----------------------------------
+
+
+      var oldDisplayMapToolDisplay = mapToolNode.style.display;
+      var oldDisplayToggleDisplay = mapTableToggleNode.style.display;
+
+      var somHandle;
+      var somTrainingData = params.table.createSOMTrainingData();
+      var map, uMatrixLayer;
+      if (somHandle) {
+        somHandle.kill();
+        somHandle = null;
+        if (map) {
+          map.destroy();
+        }
+        map = null;
+      }
+
+      var waitingDiv = document.createElement("div");
+      waitingDiv.setAttribute("data-ponder-type", "progress_indicator");
+      var spinnerIcon = document.createElement("img");
+      spinnerIcon.src = "images/ajax-loader.gif";
+      waitingDiv.appendChild(spinnerIcon);
+      var waitingDivText = document.createElement("span");
+      waitingDivText.innerHTML = "Estimating progress...";
+      waitingDiv.appendChild(waitingDivText);
+      document.getElementById("waiting").appendChild(waitingDiv);
 
       SOMFactory.SCRIPT_PATH = params.somWorkerScriptPath;
+      return SOMFactory
+      .makeSOMAsync(somTrainingData.dataArray, somTrainingData.codebookWeights)
+      .then(function (aSomHandle) {
+        somHandle = aSomHandle;
+        return somHandle.trainMap();
+      }, throwError)
+      .then(function () {
+        return somHandle.uMatrixNormalized();
+      }, throwError, function onProgress(payload) {
+        console.log('on porgress', arguments);
+        //todo!!!! YOU HACKED THE PROMISE DEPENDENCY WHICH HAD A BUG!!!!!!! FIX IT!!!!
+        waitingDivText.innerHTML = "Making map..." + Math.round(payload.progress * 100) + "%";
+      })
+      .then(function (successData) {
+
+        jquery("#map").show();
+        document.getElementById("mapToolContainer").style.display = oldDisplayMapToolDisplay;
+        mapContainer.style.display = "block";
 
 
-      return SOMFactory.makeSOMAsync(somTrainingData.dataArray, somTrainingData.codebookWeights);
+        map = new Map("map", somHandle.width, somHandle.height);
+
+        uMatrixLayer = new UMatrixTerrainLayer("ease", "easeReadout");
+        uMatrixLayer.setUMatrixData(successData.uMatrix, somHandle.width, somHandle.height);
+        map.addLayer(uMatrixLayer);
+
+        waitingDivText.innerHTML = "Finding locations ...";
+        document.getElementById("center").appendChild(waitingDiv);
 
 
+        return somHandle.bmus();
+      }, throwError)
+      .then(function (bmuResult) {
+
+        document.getElementById("toggle").style.display = oldDisplayToggleDisplay;
+
+        waitingDiv.parentNode.removeChild(waitingDiv);
+        document.getElementById("waiting").style.display = "none";
+        document.getElementById("center").style.overflow = "auto";
+
+
+        //bmus
+        var bmuLayer = new BMULayer("label", "class", "size", "sizeEasing", "legend", params.table, bmuResult.locations);
+        map.addLayer(bmuLayer);
+
+        var areaSelectLayerController = new AreaSelectLayerController();
+        areaSelectLayerController.setOnMap(map);
+
+        var bmuSelector = new BMUSelector(areaSelectLayerController, bmuLayer, somHandle, "table", "summary");
+        bmuSelector.on("RowSelection", function (object) {
+          bmuLayer.highlight([object.index]);
+        });
+
+
+        var bmuSelectionHistory = new BMUSelectionHistory("selectionHistory", bmuSelector, map, areaSelectLayerController.isActive.bind(areaSelectLayerController), [uMatrixLayer, areaSelectLayerController], "stripSelected");
+        bmuSelector.selectAll();
+
+
+        var hoverController = new HoverController();
+        hoverController.setOnMap(map);
+
+        var selectController = new SelectController();
+        selectController.setOnMap(map);
+
+
+      }, throwError)
+      .then(function () {
+
+        //start intro
+        if (getCookie("ponder-intro") === "1") {
+          return;
+        }
+
+        var intro = introJs();
+        setCookie("ponder-intro", "1", 100);
+        intro.start();
+
+
+      })
+      .then(Function.prototype, throwError);
 
 
     }
 
   };
+
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+
+    }
+    return "";
+  }
 
 
 
